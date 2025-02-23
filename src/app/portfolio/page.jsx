e7332d8a8043bd5e5220102a15fdd8b977b37bee
Saeideh Tajmehr <saeideh.tajmehr@efsibiz.com>
2025-02-23 14:55:41 +0330

    attention text

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -19,6 +19,7 @@ function Portfolio() {
       color: "from-purple-200 to-blue-100",
       title: "سایت فروشگاهی",
       desc: "پروژه سایت فروشگاهی ما با استفاده از فریمورک React توسعه یافته است تا تجربهای سریع و کارآمد برای کاربران فراهم کند. این سایت با طراحی واکنشگرا، به خوبی در دستگاههای مختلف از جمله موبایل، تبلت و دسکتاپ نمایش داده میشود. هدف ما ایجاد یک پلتفرم خرید آنلاین است که به کاربران امکان میدهد به راحتی محصولات مورد نظر خود را جستجو، مشاهده و خریداری کنند. با استفاده از تکنولوژیهای پیشرفته و رابط کاربری کاربرپسند، سایت ما تجربه خریدی لذتبخش و بدون مشکل را برای مشتریان فراهم میکند.",
+      att:'برای دیدن سایت با داده های واقعی متصل به دیتابیس لطفافیلترشکن خود را روشن کنید.',
       img: "/reactSite.png",
       link: "https://sohoshop.netlify.app",
     },
@@ -27,6 +28,7 @@ function Portfolio() {
       color: "from-blue-100 to-blue-200",
       title: " پروژه مدریت سیستم آموزشی باNextJs",
       desc: "پروژه سایت پنل آموزشی ما با هدف ارائه یک پلتفرم جامع و کارآمد برای مدیریت و آموزش طراحی شده است. این سایت دارای داشبوردهای مجزا برای مدیران و معلمان است که به هر گروه امکان میدهد به طور مستقل و موثر وظایف خود را مدیریت کنند. با استفاده از تقویم داخلی، کاربران میتوانند رویدادها و جلسات را به راحتی ثبت و پیگیری کنند. همچنین، نمودارهای پیشرفتهای در سایت تعبیه شده است که به تحلیل و بررسی دادهها کمک میکند و اطلاعات مفیدی را برای بهبود فرآیندهای آموزشی فراهم میآورد. این ویژگیها در کنار طراحی کاربرپسند و واکنشگرا، تجربهای بینظیر را برای کاربران فراهم میکند.",
+      att:'',
       img: "/school.png",
       link: "https://sohoschool.saeidehtajmehr.me/",
     },
@@ -35,6 +37,7 @@ function Portfolio() {
       color: "from-blue-200 to-violet-200",
       title: "پروژه سایت معرفی با ReactJS",
       desc: "پروژه سایت پورتفولیو ما با استفاده از فریمورک React طراحی و توسعه یافته است تا تجربهای سریع و روان برای کاربران فراهم کند. این سایت به گونهای طراحی شده که نمونه کارها و پروژههای شما را به بهترین شکل ممکن نمایش دهد. با استفاده از طراحی مدرن و واکنشگرا، سایت ما به خوبی در دستگاههای مختلف نمایش داده میشود و به کاربران امکان میدهد به راحتی با محتوای شما ارتباط برقرار کنند. هدف ما ایجاد یک پلتفرم حرفهای و جذاب است که تواناییها و دستاوردهای شما را به نمایش بگذارد و به شما کمک کند تا در دنیای دیجیتال برجسته شوید.",
+      att:'',
       img: "/portfolio.png",
       link: "https://sohoportfolio.saeidehtajmehr.me/",
     },
@@ -43,6 +46,7 @@ function Portfolio() {
       color: "from-violet-200 to-purple-200",
       title: "پنل کاربری سایت فروشگاهی",
       desc: "پروژه سایت پنل مدیریت ما با استفاده از فریمورک React توسعه یافته است و دارای امکاناتی جامع برای مدیریت محصولات، سفارشات و تحلیل دادهها میباشد. این سایت با طراحی واکنشگرا، تجربهای بینظیر را در تمامی دستگاهها از جمله موبایل، تبلت و دسکتاپ فراهم میکند. پنل مدیریت سایت به مدیران امکان میدهد تا به راحتی محصولات را اضافه، ویرایش و حذف کنند، سفارشات را پیگیری کنند و با استفاده از ابزارهای تحلیل داده، عملکرد فروشگاه را بهبود بخشند. هدف ما ایجاد یک پلتفرم مدیریت آنلاین کاربرپسند و کارآمد است که نیازهای مدیران را به بهترین شکل ممکن برآورده کند.",
+      att:'برای دیدن سایت با داده های واقعی متصل به دیتابیس لطفافیلترشکن خود را روشن کنید.',
       img: "/reactPanel.png",
       link: "https://sohoedashboard.saeidehtajmehr.me/",
     },
@@ -75,15 +79,16 @@ function Portfolio() {
           <React.Fragment key={index}>
             <div className="w-[100vw] h-screen  items-center">
               <div className= 'w-full  items-center'>
-                <div className=" h-screen flex sm:flex-col xl:flex-row  text-[#634a49] ">
-                  <div className=' xl:w-2/5 sm:w-screen p-20 shadow  bg-[#b29e9a]'>
+                <div className=" h-screen flex flex-col sm:flex-col xl:flex-row  text-[#634a49] ">
+                  <div className=' xl:w-2/5 w-screen sm:w-screen p-20 shadow  bg-[#b29e9a]'>
                     <h1 className="font-bold pb-5 text-2xl">{item.title}</h1>
                     <div className="">
                       <Image src={item.img} className="rounded-xl portBoxShadow" alt="" width={400} height={350} />
                     </div>
                   </div>
-                  <div className= 'p-20 sm:w-screen flex flex-col justify-between xl:w-3/5 '>
+                  <div className= 'p-20 w-screen sm:w-screen flex flex-col justify-between xl:w-3/5 '>
                     <p className="py-10 text-xl 2xl:text-2xl">{item.desc}</p>
+                    <p className="py-10 text-red-900 text-xl 2xl:text-2xl">{item.att}</p>
                     <Link
                       href={item.link}
                       className="w-full flex justify-end items-center"
